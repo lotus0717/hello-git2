181e94b66303fde9d9a3e17e84dfba81bdc46042
Stella dream2 <hsduswl7@naver.com>
2018-10-25 16:48:54 +0900

    Second Commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args){
       System.out.println("Hello, world from Git");
+      System.out.println("Changes after First commit"); //커밋을 수정함
     }
 }
